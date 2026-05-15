e2ae9fd47fc1f5f0ba1198ff7ac563a359ce1ead
Arpit Kumar <79630115+arpitrajput007@users.noreply.github.com>
2026-05-16 02:04:21 +0530

    chore: clean up test files

--- test-sync.js
+++
@@ -1,6 +0,0 @@
-import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
-require('dotenv').config();
-const { syncStoreData } = require('./server/syncService.js');
-const storeId = '3b79f709-28c2-42f6-b33a-8ca235f2c051';
-syncStoreData(storeId).then(() => console.log('Done test sync')).catch(console.error);
